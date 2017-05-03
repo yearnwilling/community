d6494e1a680e7adf4b46a0c3d1c5fbb382654470
weixianwei <weixianwei4@126.com>
2017-05-03 11:04:17 +0800

    layout change

--- resources/views/layout/dashboard.blade.php
+++ resources/views/layout/dashboard.blade.php
@@ -31,41 +31,17 @@ desired effect
             @section('sidebar')
                 <section class="sidebar">
                     <!-- sidebar: style can be found in sidebar.less -->
-                    <!-- Sidebar user panel (optional) -->
-                    <div class="user-panel">
-                        <div class="pull-left image">
-                            <img src="../assets/img/user2-160x160.jpg" class="img-circle" alt="User Image">
-                        </div>
-                        <div class="pull-left info">
-                            <p>Alexander Pierce</p>
-                            <!-- Status -->
-                            <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
-                        </div>
-                    </div>
-
-                    <!-- search form (Optional) -->
-                    <form action="#" method="get" class="sidebar-form">
-                        <div class="input-group">
-                            <input type="text" name="q" class="form-control" placeholder="Search...">
-                            <span class="input-group-btn">
-                <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
-                </button>
-              </span>
-                        </div>
-                    </form>
-                    <!-- /.search form -->
-
                     <!-- Sidebar Menu -->
                     <ul class="sidebar-menu">
-                        <li class="header">HEADER</li>
+                        {{--<li class="header">HEADER</li>--}}
                         <!-- Optionally, you can add icons to the links -->
-                        <li class="active"><a href="#"><i class="fa fa-link"></i> <span>Link</span></a></li>
+                        <li class="active"><a href="#"><i class="glyphicon glyphicon-home"></i><span>主页</span></a></li>
                         <li><a href="#"><i class="fa fa-link"></i> <span>Another Link</span></a></li>
                         <li class="treeview">
                             <a href="#"><i class="fa fa-link"></i> <span>Multilevel</span>
                                 <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
+                                     <i class="fa fa-angle-left pull-right"></i>
+                                </span>
                             </a>
                             <ul class="treeview-menu">
                                 <li><a href="#">Link in level 2</a></li>
@@ -84,25 +60,25 @@ desired effect
             <!-- Content Header (Page header) -->
                 <section class="content-header">
                     @section('content-header')
-                    <h1>
-                        Page Header
-                        <small>Optional description</small>
-                    </h1>
-                    <ol class="breadcrumb">
-                        <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
-                        <li class="active">Here</li>
-                    </ol>
+                        <h1>
+                            Page Header
+                            <small>Optional description</small>
+                        </h1>
+                        <ol class="breadcrumb">
+                            <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
+                            <li class="active">Here</li>
+                        </ol>
                     @show
                 </section>
 
                 <!-- Main content -->
                 <section class="content">
                     @section('body-content')
-                    <div class="box">
-                        <div class="box-body">
-                            @yield('content-body-boxed')
+                        <div class="box">
+                            <div class="box-body">
+                                @yield('content-body-boxed')
+                            </div>
                         </div>
-                    </div>
                     @show
                 </section>
         @show
